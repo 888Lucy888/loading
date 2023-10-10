e601469f20c39a2561b682de260b44fe06632953
888Lucy888 <chuggaaconroy888@gmail.com>
2023-10-09 18:42:49 -0600

    Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
+// @authors Andrés Martínez, Lucía Castañeda
+// Text Animation Base from MDBootstrap.com
+// https://mdbootstrap.com/docs/react/extended/css-text-animations/
+
 function App() {
+  const [msg, setMsg] = useState("");
+
+  function startAnimation() {
+    document.getElementById("animation").classList.add("animate");
+    document.getElementById("btn").classList.add("invisible");
+    setTimeout(()=> 
+    {
+      document.getElementById("animation").classList.remove("animate");
+      if(Math.floor(Math.random()*2)===1) setMsg("Success") 
+      else {
+        setMsg("Error");
+        document.getElementById("btn").classList.remove("invisible");
+      }
+    }
+    ,1400);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={startAnimation} id="btn">Load</button>
+        <h1 id="animation">Loading</h1>
+        <h3>{msg}</h3>
     </div>
   );
 }
